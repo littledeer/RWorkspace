f32c13025323dfd08a214e4665e87b636419398d
zl <zhanglu01@L-F006608-PC.1verge.com>
2015-07-28 19:34:04 +0800

    gbdt参数调优

--- AntiCheat/src/gbdt/train.R
+++ AntiCheat/src/gbdt/train.R
@@ -5,23 +5,24 @@ setwd("F:/RWorkspace/AntiCheat")
 
 #Train Model
 dtrain <- read.table("data/gbdt/Train.txt", header=FALSE, sep=",")
+#dtrain <- read.csv("data/gbdt/Train.csv", header=TRUE)
 tlabel <- as.numeric(dtrain[[49]])
 tdata <- as.matrix(dtrain[3:48])
 
 txgmat <- xgb.DMatrix(tdata, label = tlabel)
 
 param <- list("objective" = "binary:logistic",
-              "bst:eta" = 0.05,
-              "bst:max_depth" = 10,
+              "bst:eta" = 0.1,
+              "bst:max_depth" = 5,
               "eval_metric" = "auc",
               "eval_metric" = "error",
               "silent" = 1,
               "nthread" = 4)
 watchlist <- list("train" = txgmat)
 
-nround = 3000
+nround = 2000
 
 bst = xgb.train(param, txgmat, nround, watchlist)
 
 #save model
-xgb.save(bst, 'model/gbdt/binarylogistic-005-10-3000.model')
+xgb.save(bst, 'model/gbdt/binarylogistic-01-5-2000.model')
--- AntiCheat/src/gbdt/validation.R
+++ AntiCheat/src/gbdt/validation.R
@@ -5,7 +5,7 @@ require(Matrix)
 setwd("F:/RWorkspace/AntiCheat")
 
 #load model
-bst <- xgb.load('model/gbdt/binarylogistic-005-10-1000.model')
+bst <- xgb.load('model/gbdt/binarylogistic-01-5-2000.model')
 
 dvalidation <- read.table("data/gbdt/Validation.txt", header=FALSE, sep=",")
 vlabel <- as.numeric(dvalidation[[49]])
@@ -15,8 +15,8 @@ vxgmat <- xgb.DMatrix(vdata, label = vlabel)
 
 vpred <- predict(bst, vxgmat, missing = NULL, outputmargin = FALSE, ntreelimit = NULL, predleaf = FALSE)
 
-vpred[vpred>0.005] <- 1
-vpred[vpred<=0.005] <- 0
+vpred[vpred>0.01] <- 1
+vpred[vpred<=0.01] <- 0
 vpred <- as.integer(vpred)
 vreal <- vlabel
 
